a763e7491181be97e2f01054179309b67959ef4f
Shees Sultan <codewavestudio0.1@gmail.com>
2026-07-24 15:36:11 +0530

    error fix'

--- frontend/src/components/seo/ManagedPageSeo.jsx
+++ frontend/src/components/seo/ManagedPageSeo.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import PageMeta from '../components/seo/PageMeta';
+import PageMeta from './PageMeta';
 import { seo as seoApi } from '../services/api';
 
 /**
